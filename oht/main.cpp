eba9493e68fb4925c2ce93d386e02c370085da9a
spyoon777 <spyoon777@gmail.com>
2022-06-23 17:45:23 +0900

    openholo test proj setup

---
+++ oht/main.cpp
@@ -0,0 +1,10 @@
+// Copyright(c) 2022 KOREA ELECTRONICS TECHNOLOGY INSTITUTE(KETI), All Rights Reserved.
+// Author: spyoon777
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
